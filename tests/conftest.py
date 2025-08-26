069486dd7e42899801ec1d6e4c23c69961b46701
Jesus Garcia Rojas <jegaro10@MacBook-Pro-de-Jesus.local>
2025-08-25 18:21:24 -0600

    Chrome headless

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,20 @@
 import pytest
 import datetime
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
 def driver():
     """Crea una instancia de WebDriver y la cierra al finalizar la prueba."""
     print("\nIniciando el navegador...")
-    driver = webdriver.Chrome()
     
+    # Configuración para ejecutar en modo headless
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver  # Esto es lo que se retorna a la prueba
     
     # Esta parte se ejecuta después de que la prueba termina
