fe69f4f11e5fa68865b67b499f289d617e338a21
Jesus Garcia Rojas <jegaro10@MacBook-Pro-de-Jesus.local>
2025-09-19 11:56:17 -0600

    se añade import option

--- tests/test_login_multiuser.py
+++ tests/test_login_multiuser.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import time
 
 # --- Fixture para inicializar y cerrar el navegador ---
