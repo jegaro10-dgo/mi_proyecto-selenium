46de5af0d0f98e1be1d05df03c5cd86e30b00d9a
Jesus Garcia Rojas <jegaro10@MacBook-Pro-de-Jesus.local>
2025-09-19 15:35:06 -0600

    se añade función para probar en ambos navegadores

--- tests/test_login_multiuser.py
+++ tests/test_login_multiuser.py
@@ -3,21 +3,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions # Importación agregada
 from selenium.webdriver.chrome.options import Options
 import time
+import os
 
 # --- Fixture para inicializar y cerrar el navegador ---
 @pytest.fixture(scope="module")
 def driver():
-    print("\nInicializando WebDriver en modo headless...")
-    # Configurar las opciones de Chrome para el modo headless
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    driver_instance = webdriver.Chrome(options=options)
+    browser_name = os.getenv("BROWSER", "chrome") # Valor por defecto 'chrome' si no se encuentra
+    print(f"\nInicializando WebDriver para {browser_name} en modo headless...")
+    if browser_name == "firefox":
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        driver_instance = webdriver.Firefox(options=options)
+    else: # Por defecto usará Chrome
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        driver_instance = webdriver.Chrome(options=options)
     yield driver_instance
-    print("\nCerrando WebDriver...")
+    print(f"\nCerrando WebDriver de {browser_name}...")
     driver_instance.quit()
 
 # --- Funciones de soporte para leer los datos del usuario ---
